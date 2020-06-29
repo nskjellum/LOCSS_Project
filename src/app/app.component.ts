d04e57be0dd77d6bc1acaf4788beb6f5a7bd75d7
nskjellum <nskjellum@gmail.com>
2020-06-29 17:04:08 -0400

    Revert "Revert "6/29/20 - Commit""
    This reverts commit 6216017938d07f084ae77b40d5b046c01ab7e9d3.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { NetworkService, ConnectionStatus } from './services/network.service';
 import { Component } from '@angular/core';
+
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -6,15 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { CacheService } from 'ionic-cache';
 
+import { OfflineManagerService } from './services/offline-manager.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html'
 })
 export class AppComponent {
   constructor(
+
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
+    private offlineManager: OfflineManagerService,
+    private networkService: NetworkService,
     private cache: CacheService
   ) {
     this.initializeApp();
@@ -31,6 +38,12 @@ export class AppComponent {
 
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.networkService.onNetworkChange().subscribe((status: ConnectionStatus) => {
+        if (status == ConnectionStatus.Online) {
+          this.offlineManager.checkForEvents().subscribe();
+        }
+      });
     });
   }
 }
